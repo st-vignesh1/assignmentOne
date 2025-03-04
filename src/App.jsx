5a477628548cbbbbf48bf3d49daba8ed9b5dd24e
st-vignesh1 <vignesh@volopay.co>
2025-03-04 10:55:09 +0530

    Created a TablePage component and Rendered it in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,6 @@
-import RenderTable from "./components/table/RenderTable";
-import { data, tableHeaders } from "../src/constants/Constants";
+import TablePage from "./pages/TablePage";
 
 function App() {
-  const headers = Object.keys(tableHeaders).map(
-    (header) => tableHeaders[header]
-  );
-  return (
-    <div className="box-border w-full h-[100vh] pt-20 pl-10 pr-10">
-      <RenderTable
-        headers={headers}
-        data={data}
-        headerStyle="w-10 text-left border-1 border-gray-200 p-2"
-        dataRowStyle="odd:bg-gray-300 even:bg-white"
-        dataContentStyle="border-1 border-gray-200 p-2"
-      />
-    </div>
-  );
+  return <TablePage />;
 }
 export default App;
---
+++ src/pages/TablePage.jsx
@@ -0,0 +1,21 @@
+import RenderTable from "../components/table/RenderTable";
+import { data, tableHeaders } from "../constants/Constants";
+
+function TablePage() {
+  const headers = Object.keys(tableHeaders).map(
+    (header) => tableHeaders[header]
+  );
+  return (
+    <div className="box-border w-full h-[100vh] pt-20 pl-10 pr-10">
+      <RenderTable
+        headers={headers}
+        data={data}
+        headerStyle="w-10 text-left border-1 border-gray-200 p-2"
+        dataRowStyle="odd:bg-gray-300 even:bg-white"
+        dataContentStyle="border-1 border-gray-200 p-2"
+      />
+    </div>
+  );
+}
+
+export default TablePage;
