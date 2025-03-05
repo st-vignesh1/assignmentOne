9bbd656007736368d7915ea022c0774d73676009
st-vignesh1 <vignesh@volopay.co>
2025-03-05 11:42:05 +0530

    Installed react-router-dom and successfully set up the routing for Home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,19 @@
-import TablePage from "./pages/TablePage";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import AppLayout from "./pages/AppLayout";
+import Home from "./pages/Home";
+
+
+const router = createBrowserRouter([{
+  path: "/",
+  element: <AppLayout />,
+  children: [{
+    path: "/",
+    element: <Home />
+  }]
+}]);
+
 
 function App() {
-  return <TablePage />;
+  return <RouterProvider router={router} />
 }
 export default App;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import TablePage from './TablePage'
+
+export default function Home() {
+    return (
+        <div>
+            <TablePage />
+        </div>
+    )
+}
